b7660a1178f050709df24ff2aa1ac1be82672990
sachinkreeti <sgupta@kreeti.com>
2023-08-23 15:28:13 +0530

    fetch items from the API and show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
+const GET_ITEMS = gql`
+  query GetItems {
+    items {
+      id
+      title
+      description
+      artist {
+        firstName
+        lastName
+        email
+        createdAt
+      }
+    }
+  }
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br/>
+      <DisplayItems />
     </div>
   );
 }
 
-export default App;
+function DisplayItems() {
+  const { loading, error, data } = useQuery(GET_ITEMS);
+
+  console.log('data', data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.items.map(({ id, title, description }) => (
+    <div key={id}>
+      <h3>{title}</h3>
+      <br />
+      <b>About this:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
